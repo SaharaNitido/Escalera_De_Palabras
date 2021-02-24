47e5c30ececfc1828ec2191c8778bbb251601ab5
Mulay <Mulay@DESKTOP-1DQT6OU.home>
2021-02-24 12:53:01 +0100

    Escalera de palabras

--- src/escalera_de_palabras/Escalera_De_Palabras.java
+++ src/escalera_de_palabras/Escalera_De_Palabras.java
@@ -11,11 +11,45 @@ package escalera_de_palabras;
  */
 public class Escalera_De_Palabras {
 
+        public boolean esEscaleraDePalabras( char [][] listaPalabras){
+        int contador1 = 0;
+        
+        for (int i=0; i<listaPalabras.length-1; i++){ //el bucle que procesa todas las filas
+            contador1 = 0; //inicializo el contador de diferencias antes de empezar el bucle interno
+            for (int j=0; j<listaPalabras[0].length; j++){
+                if(listaPalabras[i][j] == listaPalabras[i+1][j]){
+                System.out.println("SON IGUALES");
+                }
+                else{
+                    System.out.println("SON DISTINTOS");
+                    contador1++; //las letras son diferent4es
+                }
+                
+            }
+            System.out.println("" + contador1); 
+            if (contador1 != 1){
+                return false; //si en algún momento las palabras se diferencian en más de una letra returne false
+            }
+        }
+        
+        return true;
+    }
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+     public static void main (String[] args){
+        Escalera_De_Palabras ejercicio = new Escalera_De_Palabras();
+        char [][] listaPalabras = {
+        {'P', 'A', 'T', 'A'},
+        {'P', 'A', 'T', 'O'},
+        {'R', 'A', 'T', 'O'},
+        {'R', 'A', 'M', 'O'},
+        {'G', 'A', 'M', 'O'},
+        {'G', 'A', 'T', 'O'},
+        {'M', 'A', 'T', 'O'},
+        };
+        
+        System.out.println(ejercicio.esEscaleraDePalabras(listaPalabras));
     }
     
 }
